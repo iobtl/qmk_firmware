cf9cdb40d31fa04c35ca82dd03614db661b6fcf6
iobtl <tedmundhtl@gmail.com>
2024-04-04 17:17:11 +0800

    try fix ::

--- keyboards/fingerpunch/ffkb/rp/v1/keymaps/personal/keymap.c
+++ keyboards/fingerpunch/ffkb/rp/v1/keymaps/personal/keymap.c
@@ -102,7 +102,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = LAYOUT_ffkb(
   KC_GRV,  KC_LABK, KC_RABK, KC_LBRC, KC_RBRC, KC_AMPR,      KC_MINS, KC_7,    KC_8,    KC_9, KC_0,    KC_PIPE,
   KC_HASH, KC_EXLM, KC_DQT, KC_LPRN,  KC_RPRN, KC_UNDS,      KC_EQL,  KC_4,    KC_5,    KC_6,  KC_COLN, KC_DLR,
-  KC_CIRC, KC_AT,   QK_REP(KC_COLN), KC_LCBR, KC_RCBR, KC_ASTR,      KC_PPLS, KC_1,    KC_2,    KC_3, KC_QUES, KC_PERC,
+  KC_CIRC, KC_AT,   SS_TAP("::"), KC_LCBR, KC_RCBR, KC_ASTR,      KC_PPLS, KC_1,    KC_2,    KC_3, KC_QUES, KC_PERC,
                     _______, _______, _______, _______,      _______, _______, _______, _______
 ),
 
