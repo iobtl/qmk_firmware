b936cfcd91ec92cf9c6bd8716b77cb126e566b40
iobtl <tedmundhtl@gmail.com>
2024-04-04 18:02:20 +0800

    replace mod-shift on space

--- keyboards/fingerpunch/ffkb/rp/v1/keymaps/personal/keymap.c
+++ keyboards/fingerpunch/ffkb/rp/v1/keymaps/personal/keymap.c
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   QK_GESC,         KC_Q,      KC_W,         KC_E,         KC_R,         KC_T,         KC_Y,    KC_U,         KC_I,         KC_O,         KC_P,             KC_BSLS,
   CTL_T(KC_TAB),  KC_A,      KC_S,         KC_D,         KC_F,         KC_G,         KC_H,    KC_J,         KC_K,         KC_L,         KC_SCLN,          KC_QUOT,
   KC_LSFT,        KC_Z,      KC_X,         KC_C,         KC_V,         KC_B,         KC_N,    KC_M,         KC_COMM,      KC_DOT,       KC_SLSH,          KC_DQT,
-                  LCTL(KC_BSPC),        KC_LCMD,      LOWER,        CTL_T(KC_ENT),       SFT_T(KC_SPC),  KC_BSPC,      RAISE,        KC_UNDO
+                  LCTL(KC_BSPC),        KC_LCMD,      LOWER,        CTL_T(KC_ENT),       KC_SPC,  KC_BSPC,      RAISE,        KC_UNDO
 ),
 
 
@@ -148,3 +148,4 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record) {
 
     return true;
 }
+
