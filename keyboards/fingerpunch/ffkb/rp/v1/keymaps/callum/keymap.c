b283cfa4846b12abf8ad66251248d5579314c53b
iobtl <tedmundhtl@gmail.com>
2024-04-08 21:17:55 +0800

    fix errors

--- keyboards/fingerpunch/ffkb/rp/v1/keymaps/callum/keymap.c
+++ keyboards/fingerpunch/ffkb/rp/v1/keymaps/callum/keymap.c
@@ -51,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,         KC_Q,      KC_W,         KC_E,         KC_R,         KC_T,         KC_Y,    KC_U,             KC_I,         KC_O,         KC_P,             KC_BSLS,
   CTL_T(KC_TAB),  KC_A,      KC_S,         KC_D,         KC_F,         KC_G,         KC_H,    KC_J,             KC_K,         KC_L,         KC_SCLN,          KC_QUOT,
   KC_LSFT,          KC_Z,      KC_X,         KC_C,         KC_V,         KC_B,         KC_N,    KC_M,             KC_COMM,      KC_DOT,       KC_SLSH,          KC_RSFT,
-                  LCTL(KC_BSPC),        KC_CMD,      SYM,        KC_LSFT,         KC_SPC,  KC_BSPC,    NAV,      KC_UNDO
+                  LCTL(KC_BSPC),        KC_LGUI,      SYM,        KC_LSFT,         KC_SPC,  KC_BSPC,    NAV,      KC_UNDO
 ),
 
 /* Sym
@@ -164,7 +164,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 // used to turn on third layer if both SYM and NAV are activated
 layer_state_t layer_state_set_user(layer_state_t state) {
-  return update_tri_layer_state(state, _SYM, _NAV);
+  return update_tri_layer_state(state, _SYM, _NAV, _NUM);
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t* record) {
