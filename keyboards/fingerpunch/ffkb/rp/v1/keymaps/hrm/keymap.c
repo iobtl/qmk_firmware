bee203153f1674b32cd385bcabc7914296e9341b
iobtl <tedmundhtl@gmail.com>
2024-04-16 07:38:39 +0800

    swap sym and thumb lsft

--- keyboards/fingerpunch/ffkb/rp/v1/keymaps/hrm/keymap.c
+++ keyboards/fingerpunch/ffkb/rp/v1/keymaps/hrm/keymap.c
@@ -69,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,         KC_Q,      KC_W,         KC_E,         KC_R,         KC_T,         KC_Y,    KC_U,             KC_I,         KC_O,         KC_P,             KC_BSLS,
   KC_TAB,  KC_A,      KC_S,         KC_D,         KC_F,         KC_G,         KC_H,    KC_J,        KC_K ,         KC_L,         KC_SCLN,          KC_QUOT,
   KC_LSFT,          LCTL_T(KC_Z),      LALT_T(KC_X),         LGUI_T(KC_C),         LSFT_T(KC_V),         KC_B,         KC_N,    RSFT_T(KC_M),             RGUI_T(KC_COMM),      RALT_T(KC_DOT),       RCTL_T(KC_SLSH),          KC_RSFT,
-                  LCTL(KC_BSPC),        SYM,      KC_LSFT,        KC_ENT,         KC_SPC,  KC_BSPC,    NAV,      KC_UNDO
+                  LCTL(KC_BSPC),        KC_LSFT,      SYM,        KC_ENT,         KC_SPC,  KC_BSPC,    NAV,      KC_UNDO
 ),
 
 /* COLEMAK-DH
@@ -85,7 +85,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,         KC_Q,      KC_W,         KC_F,         KC_P,         KC_B,         KC_J,    KC_L,             KC_U,         KC_Y,         KC_SCLN,             KC_BSLS,
   KC_TAB,  KC_A,      KC_R,         KC_S,         KC_T,         KC_H,         KC_M,    KC_N,        KC_E,         KC_I,         KC_O,          KC_QUOT,
   KC_LSFT,          LCTL_T(KC_Z),      LALT_T(KC_X),         LGUI_T(KC_C),         LSFT_T(KC_D),         KC_V,         KC_K,    RSFT_T(KC_H),             RGUI_T(KC_COMM),      RALT_T(KC_DOT),       RCTL_T(KC_SLSH),          KC_RSFT,
-                  LCTL(KC_BSPC),        SYM,      KC_LSFT,        KC_ENT,         KC_SPC,  KC_BSPC,    NAV,      KC_UNDO
+                  LCTL(KC_BSPC),        KC_LSFT,      SYM,        KC_ENT,         KC_SPC,  KC_BSPC,    NAV,      KC_UNDO
 ),
 
 /* Sym
